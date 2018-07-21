a0c3775418ca38263273bb55c8da3f5acc4af7b6
Filip <filip.nilsson@blixtmail.se>
2018-07-21 17:07:21 +0200

    Struct for cpu registers

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "constants.h"
+
+typedef union {
+  struct 
+  {
+    uint8_t high;
+    uint8_t low;
+  } r8;
+  uint16_t r16;
+} reg;
+
+typedef struct {
+  reg AF;
+  reg BC;
+  reg DE;
+  reg HL;
+  reg SP;
+  reg PC;
+} cpu;
 
 int main(int argc, char* argv[])
 {
