2010e4e516df6a91c579b4f5bcbbeec6c0f4e797
Filip <filip.nilsson@blixtmail.se>
2018-10-10 01:12:55 +0200

    Remove unnecessary whitespace

--- src/utils.c
+++ src/utils.c
@@ -3,18 +3,18 @@
 
 uint16_t generate_address(uint8_t low_byte, uint8_t high_byte)
 {
-  uint16_t res = (uint16_t) high_byte << 8 | (uint16_t) low_byte; 
+  uint16_t res = (uint16_t) high_byte << 8 | (uint16_t) low_byte;
   return res;
 }
 
 void _set_flag(proc* p, uint8_t f)
-{ 
-  p->af.r8.low |= f;  
+{
+  p->af.r8.low |= f;
 }
 
 void clear_flags(proc* p, uint8_t f)
 {
-  p->af.r8.low &= ~f;  
+  p->af.r8.low &= ~f;
 }
 
 void set_flag(proc* p, uint8_t f, uint8_t val)
@@ -27,6 +27,6 @@ void set_flag(proc* p, uint8_t f, uint8_t val)
 
 uint8_t test_flag(proc* p, uint8_t f)
 {
-  return (p->af.r8.low & f) != 0;  
+  return (p->af.r8.low & f) != 0;
 }
 
